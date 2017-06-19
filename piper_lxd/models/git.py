7f353e78531307322e8562a9e04d44001e4a6e8a
Martin Franc <me@martinfranc.eu>
2017-06-19 19:13:13 +0200

    Improve git clone

--- piper_lxd/models/git.py
+++ piper_lxd/models/git.py
@@ -9,19 +9,13 @@ def clone(origin, branch, commit, destination: str, ssh_keys_path=None):
         assert type(ssh_keys_path) is list
         env['GIT_SSH_COMMAND'] = 'ssh -i ' + ' -i '.join(ssh_keys_path) + ' -F /dev/null'
 
-    command = ['git', 'clone', origin, '.']
+    command = ['git', 'clone', '--recursive', '--branch', branch, origin, '.']
     process = subprocess.Popen(command, cwd=destination, stdout=subprocess.PIPE, stderr=subprocess.PIPE, env=env)
     out, err = process.communicate()
     if process.returncode != 0:
         raise CloneException(err)
 
-    command = ['git', 'reset', '--hard', commit]
-    process = subprocess.Popen(command, cwd=destination, stdout=subprocess.PIPE, stderr=subprocess.PIPE, env=env)
-    out, err = process.communicate()
-    if process.returncode != 0:
-        raise CloneException(err)
-
-    command = ['git', 'submodule', 'update', '--init', '--recursive']
+    command = ['git', 'checkout', '-f', commit]
     process = subprocess.Popen(command, cwd=destination, stdout=subprocess.PIPE, stderr=subprocess.PIPE, env=env)
     out, err = process.communicate()
     if process.returncode != 0:
